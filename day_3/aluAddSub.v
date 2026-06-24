22c15dbe7d56e56556dc8a0603435a7091a62b04
skaustubh04 <kaustubhshingate04@gmail.com>
2026-06-25 00:58:38 +0530

    Corrections in Day 3 code; added images

--- day_3/aluAddSub.v
+++ day_3/aluAddSub.v
@@ -10,20 +10,22 @@ module alu_add_sub #(parameter WIDTH=4)(a, b, cb_in, mode, sum, cb_out);
 	output reg [WIDTH-1:0] sum;
 	output reg 	       cb_out;	// Carry out, Borrow out
 
-	reg [WIDTH:0] result_reg;
-
 	always @(*) begin
 		if (mode == 1'b0) begin
 			// ADD
-			result_reg = a + b + cb_in;
+			{cb_out, sum} = a + b + cb_in;
+		end
+		else if (mode == 1'b1) begin
+			// SUB
+			{cb_out, sum} = a - b - cb_in;
 		end
 		else begin
-			// SUB
-			result_reg = a - b - cb_out;
+			// explicilty mentioning unknown values
+			// REASON: if not done, 'sum' and 'cb_out' will take
+			// unknown values, but won't be able to recover from them.
+			{cb_out, sum} = {(WIDTH+1){1'bx}};
 		end
 
-		sum = 	 result_reg[WIDTH-1:0];
-		cb_out = result_reg[WIDTH];
 	end
 
 endmodule
--- day_3/aluAddSubTB.v
+++ day_3/aluAddSubTB.v
@@ -2,7 +2,7 @@
 
 module alu_add_sub_tb;
 
-	parameter WIDTH=4;
+	parameter WIDTH = 4;
 
 	reg [WIDTH-1:0]  a, b;
 	reg 		 cb_in, mode;
@@ -10,57 +10,62 @@ module alu_add_sub_tb;
 	wire [WIDTH-1:0] sum;
 	wire		 cb_out;
 
+	// variables not part of the uut
 	reg [WIDTH-1:0] a_reg [3:0];
 	reg [WIDTH-1:0] b_reg [3:0];
 	reg [3:0] 	cb_in_reg;
 
-	alu_add_sub uut #(
+	integer i;						// used in for-loop
+
+	// parameterized uut
+	alu_add_sub #(
 		.WIDTH(WIDTH)
-	) (
+	) uut (
 		.a(a), .b(b), .cb_in(cb_in), .mode(mode),
 		.sum(sum), .cb_out(cb_out)
 	);
 
+	// passing values
 	initial begin
-		$dumpfile ("aluAddSubTB.vcd");
-		$dumpvars (0, alu_add_sub_tb);
+		$dumpfile ("aluAddSubTB.vcd");			// dumpfile which will hold output
+		$dumpvars (0, alu_add_sub_tb);			// variables from this file
 
 		$display ("\n");
-		$monitor ("a=%b, b=%b, cb_in=%b, sum=%b, cb_out=%b", a, b, cb_in, sum, cb_out);
+		$monitor ("a=%b, b=%b, cb_in=%b, sum=%b, cb_out=%b", a, b, cb_in, sum, cb_out);	// monitoring these variables
 
-		$display("mode=0 (ADD)");
-		integer i;
-		mode=1'b0;
+		$display ("mode=0 (ADD)");
+		mode=1'b0;					// setting 'mode' as '0' -> ADD
 		for (i=0; i<4; i=i+1) begin
-			a = $urandom_range (0, 2**(WIDTH)-1);
-			b = $urandom_range (0, 2**(WIDTH)-1);
-			cb_in = $urandom_range (0, 1);
+			a = $urandom_range (0, 2**(WIDTH)-1);	// random 4-bit number, range: (0, 15)
+			b = $urandom_range (0, 2**(WIDTH)-1);	// random 4-bit number, range: (0, 15)
+			cb_in = $urandom_range (0, 1);		// random 1-bit number, range: (0, 1)
 
-			a_reg[i] = a;
-			b_reg[i] = b;
+			a_reg[i] = a;				// storing these values to...
+			b_reg[i] = b;				// ...compare with SUB later
 			cb_in_reg[i] = cb_in;
 
 			#10;
 		end
 
-		$display ("\ntransitioning from ADD to SUB");
-		mode=1'bx;
-
-		$display("\nmode=1 (SUB)");
-		integer i;
-		mode=1'b1;
+		$display ("\nTransitioning from ADD to SUB");
+		mode =  1'bx; 					// unknown value while transitioning
+		a = {WIDTH{1'bx}};				// assign 1'bx, 'WIDTH' amount of times
+		b = {WIDTH{1'bx}};				// assign 1'bx, 'WIDTH' amount of times
+		cb_in = 1'bx;
+		#10;
+		
+		$display ("\nmode=1 (SUB)");
+		mode=1'b1;					// setting 'mode' as '1' -> SUB
 		for (i=0; i<4; i=i+1) begin
-			a = a_reg[i];
-			b = b_reg[i];
+			a = a_reg[i];				// using the previously stored values...
+			b = b_reg[i];				// ...so that comparison is easier
 			cb_in = cb_in_reg[i];
-
-			a_reg[i] = a;
-			b_reg[i] = b;
-			cb_in_reg[i] = cb_in;
-
+			
 			#10;
 		end
 	
+		$display ("\n");
 		$finish;
+	end
 
 endmodule		
