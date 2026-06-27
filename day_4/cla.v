129cb4883a164511fddc8db7032b7dbbdcead8e6
skaustubh04 <kaustubhshingate04@gmail.com>
2026-06-27 11:58:01 +0530

    Corrected CLA code (Design)

--- day_4/cla.v
+++ day_4/cla.v
@@ -4,7 +4,7 @@ module cla (
 	input [3:0]  a,
 	input [3:0]  b,
 	input 	     c_in,
-	output [3:0] sum,
+	output reg [3:0] sum,
 	output reg   c_out
 );
 
@@ -14,13 +14,22 @@ module cla (
 	assign p = a ^ b;
 	assign g = a & b;
 
-	assign sum = p ^ c_in;
-
 	always @(*) begin
-		c[0] = g[0] + (p[0] & c_in);
-		c[1] = g[1] + (p[1] & g[0]) + (p[1] & p[0] & c_in);
-		c[2] = g[2] + (p[2] & g[1]) + (p[2] & p[1] & g[0]) + (p[2] & p[1] & p[0] & c_in);
-		c_out = g[3] + (p[3] & g[2]) + (p[3] & p[2] & g[1]) + (p[3] & p[2] & p[1] & g[0]) + (p[3] & p[2] & p[1] & p[0] & c_in);
+		
+		c[1] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & c_in);
+		c[2] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & c_in);
+		c_out = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & c_in);
+
+		// Fixed a mistake:
+		// Earlier, just before the 'always' block, i had written 'assign sum = p ^ c_in'
+		// This would provide incorrect result as the further carry
+		// aren't getting used, and only 'c_in' gets extended to
+		// 4 bits (like 4'b000c_in), which is incorrect
+
+		sum[0] = p[0] ^ c_in;
+		sum[1] = p[1] ^ c[0];
+		sum[2] = p[2] ^ c[1];
+		sum[3] = p[3] ^ c[2];
 	end
 
 endmodule
